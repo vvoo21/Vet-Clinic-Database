9519278524a251e2d6e83c575b09e5f50e188508
Vanessa O <vaneolipa@gmail.com>
2023-03-28 19:06:01 -0500

    Transactions to INSERT, UPDATE DELETE data. Add queries

--- queries.sql
+++ queries.sql
@@ -31,3 +31,110 @@ WHERE name != 'Gabumon';
 SELECT *
 FROM animals 
 WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- 1st transaction
+BEGIN;
+
+UPDATE animals
+SET species = 'unspecified';
+
+SELECT *
+FROM animals;
+
+ROLLBACK;
+
+SELECT *
+FROM animals;
+
+-- 2nd transaction
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+SELECT *
+FROM animals;
+
+COMMIT;
+
+SELECT *
+FROM animals;
+
+-- 3rd transaction
+BEGIN;
+
+DELETE FROM animals;
+
+SELECT *
+FROM animals;
+
+ROLLBACK;
+
+SELECT *
+FROM animals;
+
+-- 4th transaction
+BEGIN;
+
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+SELECT *
+FROM animals;
+
+ROLLBACK TO SP1;
+
+SELECT *
+FROM animals;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+SELECT *
+FROM animals;
+
+COMMIT;
+
+SELECT *
+FROM animals;
+
+-- More queries
+SELECT COUNT(*) AS animals
+FROM animals;
+
+SELECT COUNT(*) AS animals_never_tried_to_escape
+FROM animals
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) AS average_weight_of_animals
+FROM animals;
+
+SELECT neutered, SUM(escape_attempts) AS total_escape_attempts
+FROM animals
+GROUP BY neutered
+ORDER BY SUM(escape_attempts) DESC
+LIMIT 1;
+
+SELECT species AS type_of_animal, MIN(weight_kg) AS min_weight_kg, MAX(weight_kg) AS max_weight_kg
+FROM animals
+GROUP BY type_of_animal;
+
+SELECT species AS type_of_animal, AVG(escape_attempts) AS average_of_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY type_of_animal;
+
+
+
+
